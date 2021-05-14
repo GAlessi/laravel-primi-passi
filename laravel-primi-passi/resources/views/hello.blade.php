b69719487b99138f453731256cf8a33b70934034
Gabriele Alessi <89archaon89@gmail.com>
2021-05-14 16:05:16 +0200

    esercizio concluso

---
+++ laravel-primi-passi/resources/views/hello.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Hello World</h1>
+        </div>
+    </body>
+</html>
---
+++ laravel-primi-passi/resources/views/mainPage.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <a href="/hello"> <h1 class="button">
+                Pagina esercizio
+            </h1></a>
+        </div>
+    </body>
+</html>
